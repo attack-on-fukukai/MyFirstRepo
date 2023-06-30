02a05a9e581aa1a95bc4d49924470f44069028dd
attack-on-fukukai <attack.on.fukukai+github@gmail.com>
2023-06-30 09:50:13 +0900

    掛け算機能を追加

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ def sub(a, b):
     return a-b
 
 
+def mul(a, b):
+    return a*b
+
+
 def main():
-    print(add(1, 2))
-    print(sub(2, 1))
+    print(f"{1} + {2} = {add(1,2)}")
+    print(f"{2} - {1} = {sub(1,2)}")
+    print(f"{2} * {2} = {mul(2,2)}")
 
 
 if __name__ == '__main__':
